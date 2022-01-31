ac1a2b7f423f91c60c52034e7d9071907ea027b3
forysiak-vova <volodja.forus@gmail.com>
2022-01-31 20:31:15 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ state = {
+  good: 0,
+  neutral: 0,
+   bad: 0,
+ }
+
+  onClickBtnGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1
+    }))
+  }
+  
+   onClickBtnNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1
+    }))
+   }
+   onClickBtnBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1
+    }))
+   }
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+   countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+
+    return (
+      <>
+         <section>
+          <h1>Please leave feedback</h1>
+          <div>
+            <button onClick={this.onClickBtnGood}>good</button>
+            <button onClick={this.onClickBtnNeutral}>neutral</button>
+           <button onClick={this.onClickBtnBad}>bad</button>
+          </div>
+          <h2>Statistis</h2>
+          <ul>
+            <li>good: {this.state.good}</li>
+            <li>neutral: {this.state.neutral}</li>
+            <li>bad: {this.state.bad}</li>
+          </ul>
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+           
+      </section>
+      </>
+    )
+  }
 }
 
 export default App;
